ee96d6e6baa1be9536cdd2512469ccebad90f160
Alexandre Belchior <alexbelchior@gmail.com>
2017-12-11 16:54:54 -0200

    Update principal.js

--- Curso Alura 2017/js/principal.js
+++ Curso Alura 2017/js/principal.js
@@ -1,31 +1,37 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for(var i = 0; i<pacientes.length; i++){
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+  var paciente = pacientes[i];
 
-var tdImc = paciente.querySelector(".info-imc");
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
 
-if(peso < 0 || peso > 1000){
-  console.log("Peso inválido!");
-  tdImc.textContent = "Peso inválido";
-  pesoEhValido = false;
-}
-if(altura < 0 || altura > 3.00){
-  console.log("Altura inválida!");
-  tdImc.textContent = "Altura Inválida";
-  alturaEhValida = false;
-}
+  var tdImc = paciente.querySelector(".info-imc");
 
-if(alturaEhValida && pesoEhValido){
-  var imc = peso / (altura * altura);
-  tdImc.textContent = imc;
+  var pesoEhValido = true;
+  var alturaEhValida = true;
+
+  if(peso < 0 || peso > 1000){
+    console.log("Peso inválido!");
+    tdImc.textContent = "Peso inválido";
+    pesoEhValido = false;
+  }
+  if(altura < 0 || altura > 3.00){
+    console.log("Altura inválida!");
+    tdImc.textContent = "Altura Inválida";
+    alturaEhValida = false;
+  }
+
+  if(alturaEhValida && pesoEhValido){//verificar se a altura e peso são true
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc.toFixed(2);//limita para exibir até duas casas decimais
+  }
+
 }
