d84bfa7886efc691da802ca520fc556bb63d807b
Alexandre Belchior <alexbelchior@gmail.com>
2017-12-18 16:24:16 -0200

    Create form.js

---
+++ Curso Alura 2017/js/form.js
@@ -0,0 +1,33 @@
+botaoAdicionar.addEventListener("click", function(event){
+  event.preventDefault();//Elimina o comportamento padrão do botão que está dentro do form de recarregar a página.
+  console.log("Fui clicado!");
+
+  var form = document.querySelector("#form-adiciona");
+
+  var nome = form.nome.value;
+  var peso = form.peso.value;
+  var altura = form.altura.value;
+  var gordura = form.gordura.value;
+
+  var pacienteTr = document.createElement("tr");
+
+  var nomeTd = document.createElement("td");
+  var pesoTd = document.createElement("td");
+  var alturaTd = document.createElement("td");
+  var gorduraTd = document.createElement("td");
+  var imcTd = document.createElement("td");
+
+  nomeTd.textContent = nome;
+  pesoTd.textContent = peso;
+  alturaTd.textContent = altura;
+  gorduraTd.textContent = gordura;
+
+  pacienteTr.appendChild(nomeTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+
+  var tabela = document.querySelector("#tabela-pacientes");
+
+  tabela.appendChild(pacienteTr);
+});
