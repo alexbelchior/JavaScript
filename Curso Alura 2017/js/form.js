cd1b5d625523532e5c64c41c5f4e1bb20c477072
Alexandre Belchior <alexbelchior@gmail.com>
2018-01-30 22:39:30 -0200

    Update form.js

--- Curso Alura 2017/js/form.js
+++ Curso Alura 2017/js/form.js
@@ -10,11 +10,23 @@ botaoAdicionar.addEventListener("click", function(event){
 
   var pacienteTr = montaTr(paciente);
 
+  var erros = validaPaciente(paciente);
+
+  console.log(erros);
+
+  if(erros.length > 0){
+    exibeMensagensDeErro(erros);
+    return; //Return vazio o código não irá executar o restante dentro da função do click do botão
+  }
+
   var tabela = document.querySelector("#tabela-pacientes");
 
   tabela.appendChild(pacienteTr);
 
   form.reset();
+
+  var mensagensErro = document.querySelector("#mensagens-erro");
+  mensagensErro.innerHTML = "";
 });
 
 
@@ -56,4 +68,47 @@ function montaTd(dado, classe){
   td.classList.add(classe);
 
   return td;
+}
+
+function validaPaciente(paciente){
+
+  var erros = [];
+
+  if (paciente.nome.length == 0) {
+    erros.push("O nome não pode ser em branco");
+  }
+
+  if (paciente.gordura.length == 0){
+    erros.push("A gordura não pode ser em branco");
+  }
+
+  if (paciente.peso.length == 0){
+    erros.push("O peso não pode ser em branco");
+  }
+
+  if (paciente.altura.length == 0){
+    erros.push("A altura não pode ser em branco");
+  }
+
+  if(!validaPeso(paciente.peso)){
+    erros.push("Peso inválido!");
+  }
+
+  if(!validaAltura(paciente.altura)){
+    erros.push("Altura inválida!");
+  }
+
+  return erros;
+}
+
+function exibeMensagensDeErro(erros){
+  var ul = document.querySelector("#mensagens-erro");
+  ul.innerHTML = "";
+
+  erros.forEach(function(erro){
+    var li = document.createElement("li");
+    li.textContent = erro;
+    ul.appendChild(li);
+
+  });
 }
