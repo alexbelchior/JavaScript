5046627a6cbd953f35e704e2ed7ae8e53d6208ee
Alexandre Belchior <alexbelchior@gmail.com>
2018-01-30 22:38:48 -0200

    Update calcula-imc.js

--- Curso Alura 2017/js/calcula-imc.js
+++ Curso Alura 2017/js/calcula-imc.js
@@ -15,16 +15,16 @@ for(var i = 0; i<pacientes.length; i++){
 
   var tdImc = paciente.querySelector(".info-imc");
 
-  var pesoEhValido = true;
-  var alturaEhValida = true;
+  var pesoEhValido = validaPeso(peso);
+  var alturaEhValida = validaAltura(altura);
 
-  if(peso < 0 || peso > 1000){
+  if(!pesoEhValido){
     console.log("Peso inválido!");
     tdImc.textContent = "Peso inválido";
     pesoEhValido = false;
     paciente.classList.add("paciente-invalido");
   }
-  if(altura < 0 || altura > 3.00){
+  if(!alturaEhValida){
     console.log("Altura inválida!");
     tdImc.textContent = "Altura Inválida";
     alturaEhValida = false;
@@ -44,4 +44,21 @@ function calculaImc(peso, altura){
   imc = peso / (altura * altura);
 
   return imc.toFixed(2);//limita para exibir até duas casas decimais
+}
+
+function validaPeso(peso){
+  if(peso > 0 && peso < 300){
+    return true;
+  }else{
+    return false;
+  }
+
+}
+
+function validaAltura(altura){
+  if(altura > 0 && altura < 3.00){
+    return true;
+  }else{
+    return false;
+  }
 }
